ea905018069440de105fa89ae161064804a6a51a
Brian W. Wolter <brian@brianwolter.com>
2017-01-05 10:15:28 -0500

    Adding singular tests

--- roles_test.go
+++ roles_test.go
@@ -12,18 +12,35 @@ type rolesExample struct {
   C int `json:"c"`
 }
 
+type singularExample struct {
+  A int `json:"a" role:"public,private"`
+  B int `json:"b" role:"private"`
+  C int `json:"c"`
+}
+
 func TestBasicRoles(t *testing.T) {
   var data []byte
   var err error
   
-  ex := rolesExample{1, 2, 3}
+  ex1 := rolesExample{1, 2, 3}
+  ex2 := singularExample{1, 2, 3}
   
-  data, err = MarshalRole("public", ex)
+  data, err = MarshalRole("public", ex1)
   if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
     assert.Equal(t, `{"a":1,"c":3}`, string(data))
   }
   
-  data, err = MarshalRole("private", ex)
+  data, err = MarshalRole("public", ex2)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, `{"a":1,"c":3}`, string(data))
+  }
+  
+  data, err = MarshalRole("private", ex1)
+  if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
+    assert.Equal(t, `{"a":1,"b":2,"c":3}`, string(data))
+  }
+  
+  data, err = MarshalRole("private", ex2)
   if assert.Nil(t, err, fmt.Sprintf("%v", err)) {
     assert.Equal(t, `{"a":1,"b":2,"c":3}`, string(data))
   }
